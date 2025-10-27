c7ad5ce13927ab850285bd7a134aa640ce4092ec
Timothy Riordan <tmiker2004@yahoo.com>
2025-10-26 19:56:56 -0500

    Added DevTestsController action to test GlobalExceptionHandlingMiddleware in Products.Read.API.

--- Products.Read.API/Controllers/DevTestsController.cs
+++ Products.Read.API/Controllers/DevTestsController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Products.Read.API.Configuration;
+using Products.Read.API.DTOs.DevTests;
+using Products.Read.API.Exceptions;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Products.Read.API.Controllers
 {
@@ -15,6 +18,30 @@ namespace Products.Read.API.Controllers
         {
             _cloudAmqpSettings = cloudAmqpSettings;
             _logger = logger;
+        }
+
+        [HttpPost("throwExceptionForTesting")]
+        public IActionResult ThrowExceptionForTesting([FromBody] ThrowExceptionDTO throwExceptionDTO, CancellationToken cancellationToken)
+        {
+            // Note passing Correlation ID from the request headers to the command as Microsoft recommends
+            // caution using IHttpContextAccessor to get http context if want to pull header in handlers
+            // (https://learn.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.http.ihttpcontextaccessor?view=aspnetcore-9.0).
+
+            Exception ex = throwExceptionDTO.ExceptionType.ToLower() switch
+            {
+                "validationexception" => throw new ValidationException("This is a test ValidationException thrown from ThrowExceptionHandler."),
+                "unauthorizedaccessexception" => throw new UnauthorizedAccessException("This is a test UnauthorizedAccessException thrown from ThrowExceptionHandler."),
+                "forbiddenexception" => throw new ForbiddenException("This is a test ForbiddenException thrown from ThrowExceptionHandler."),
+                "notfoundexception" => throw new NotFoundException("This is a test NotFoundException thrown from ThrowExceptionHandler."),
+                "conflictexception" => throw new ConflictException("This is a test ConflictException thrown from ThrowExceptionHandler."),
+                "argumentexception" => throw new ArgumentException("This is a test ArgumentException thrown from ThrowExceptionHandler."),
+                "argumentnullexception" => throw new ArgumentNullException("This is a test ArgumentNullException thrown from ThrowExceptionHandler."),
+                "invalidoperationexception" => throw new InvalidOperationException("This is a test InvalidOperationException thrown from ThrowExceptionHandler."),
+                "taskcanceledException" => throw new TaskCanceledException("This is a test TaskCanceledException thrown from ThrowExceptionHandler."),
+                _ => throw new Exception("This is a test general Exception thrown from ThrowExceptionHandler.")
+            };
+
+            return Ok();
         }
 
         [HttpGet("getCloudAmqpSettingsTestingDummyValue")]
---
+++ Products.Read.API/DTOs/DevTests/ThrowExceptionDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Products.Read.API.DTOs.DevTests
+{
+    public class ThrowExceptionDTO
+    {
+        public string ExceptionType { get; init; } = default!;
+
+        public ThrowExceptionDTO(string exceptionType)
+        {
+            ExceptionType = exceptionType;
+        }
+    }
+}
