807a54dc5d3e9c64751a0561301f1c022aff39c4
Timothy Riordan <tmiker2004@yahoo.com>
2025-11-30 15:37:47 -0600

    Configured Blazor Admin Client to use YARP proxy. Note the Dev/Test Client accesses APIs directly - it does NOT use YARP.  Configured IDP for automatic redirect to application on logout.

--- Development.Blazor/Development.Blazor.Client/Utility/StaticData.cs
+++ Development.Blazor/Development.Blazor.Client/Utility/StaticData.cs
@@ -7,31 +7,31 @@
         public const string ProductsReadHttpClient_ClientName = "ProductsReadHttpClient";
         public const string ProductsReadHttpClient_BaseURL = "https://localhost:7101";
         public const string ProductsReadHttpClient_ProductsPath = "/api/products";
-        public const string ProductsReadHttpClient_DevTestsPath = "/api/devTests";
+        public const string ProductsReadHttpClient_DevTestsPath = "/api/products/devTests";
         public const string ProductsReadHttpClient_GetApiUserInfoSubpath = "/getApiUserInfo";
 
         public const string CartsHttpClient_ClientName = "CartsHttpClient";
         public const string CartsHttpClient_BaseURL = "https://localhost:7184";
         public const string CartsHttpClient_CartsPath = "/api/carts";
-        public const string CartsHttpClient_DevTestsPath = "/api/devTests";
+        public const string CartsHttpClient_DevTestsPath = "/api/carts/devTests";
         public const string CartsHttpClient_GetApiUserInfoSubpath = "/getApiUserInfo";
 
         public const string ProductsWriteHttpClient_ClientName = "ProductsWriteHttpClient";
         public const string ProductsWriteHttpClient_BaseURL = "https://localhost:7213";
         public const string ProductsWriteHttpClient_ProductsPath = "/api/productsManagement";
-        public const string ProductsWriteHttpClient_DevTestsPath = "/api/devTests";
+        public const string ProductsWriteHttpClient_DevTestsPath = "/api/productsManagement/devTests";
         public const string ProductsWriteHttpClient_GetApiUserInfoSubpath = "/getApiUserInfo";
 
         public const string AccountsHttpClient_ClientName = "AccountsHttpClient";
         public const string AccountsHttpClient_BaseURL = "https://localhost:7033";
         public const string AccountsHttpClient_AccountsPath = "/api/accounts";
-        public const string AccountsHttpClient_DevTestsPath = "/api/devTests";
+        public const string AccountsHttpClient_DevTestsPath = "/api/accounts/devTests";
         public const string AccountsHttpClient_GetApiUserInfoSubpath = "/getApiUserInfo";
 
         public const string OrdersHttpClient_ClientName = "OrdersHttpClient";
         public const string OrdersHttpClient_BaseURL = "https://localhost:7019";
         public const string OrdersHttpClient_OrdersPath = "/api/orders";
-        public const string OrdersHttpClient_DevTestsPath = "/api/devTests";
+        public const string OrdersHttpClient_DevTestsPath = "/api/orders/devTests";
         public const string OrdersHttpClient_GetApiUserInfoSubpath = "/getApiUserInfo";
     }
 }
