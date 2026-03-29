aeadc2b4d76652125118f4b0a18559864db83100
Timothy Riordan <tmiker2004@yahoo.com>
2026-03-29 15:39:52 -0500

    Added order details component in Consumer Blazor Client.

--- Consumer.Blazor/Consumer.Blazor.Client/DTOs/Accounts/AddressDTO.cs
+++ Consumer.Blazor/Consumer.Blazor.Client/DTOs/Accounts/AddressDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace Consumer.Blazor.Client.DTOs.Accounts
+﻿using System.Text;
+
+namespace Consumer.Blazor.Client.DTOs.Accounts
 {
     public class AddressDTO
     {
@@ -9,5 +11,16 @@
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(Street1);
+            if (!string.IsNullOrWhiteSpace(Street2)) sb.Append($", {Street2}");
+            sb.Append($", {City}, {State} {PostalCode}");
+            return sb.ToString();
+        }
     }
 }
+
+    
